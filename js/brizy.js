5344fd4196001770e80adaee7417f48b23a09189
64bakerst <108701366+64bakerst@users.noreply.github.com>
2024-01-18 18:07:13 -0800

    Update brizy.js

--- js/brizy.js
+++ js/brizy.js
@@ -1,6 +1,6 @@
-    // JavaScript code to test if it's loading
-    document.addEventListener('DOMContentLoaded', function() {
-        // Code to run after the DOM has loaded
-        console.log('JavaScript is loaded!');
-        alert('JavaScript is loaded!');
-    });
+        // JavaScript code to test if it's loading
+        document.addEventListener('DOMContentLoaded', function() {
+            // Code to run after the DOM has loaded
+            console.log('JavaScript is loaded!');
+            alert('JavaScript is loaded!');
+        });
