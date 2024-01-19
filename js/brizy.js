fcb961e1b44caadbe3782d59b847710fa77484e5
64bakerst <108701366+64bakerst@users.noreply.github.com>
2024-01-18 19:28:21 -0800

    Update brizy.js

--- js/brizy.js
+++ js/brizy.js
@@ -1,5 +1,4 @@
-        // JavaScript code to test if it's loading
-        document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('DOMContentLoaded', function() {
             // Code to run after the DOM has loaded
             console.log('JavaScript is loaded!');
             alert('JavaScript is loaded!');
